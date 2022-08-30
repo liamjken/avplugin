d3d5d2bbfd31e35280bf28425dd7c99248b6c339
liam kennedy <liamjken@gmail.com>
2022-08-30 11:10:38 -0400

    removing incorrect SDK

--- includes/av-sdk-setup.php
+++ includes/av-sdk-setup.php
@@ -5,6 +5,6 @@ function autoverify_sdk_wp_head(){
     //Close PHP tags 
     ?>
     <!-- This is the SDK setup without Attributes and is no longer required -->
-  <!--  <script async defer src="https://sdk.autoverify.com/09182288-08d7-427e-8256-725bbd593e18/162c4d71-4d82-40d4-a1aa-a747ffbf12a8/sdk.min.js"></script>-->
+  <!--  <script async defer src="<?php echo esc_attr($sdkurl); ?>"></script>-->
     <?php //Open PHP tags
 }
