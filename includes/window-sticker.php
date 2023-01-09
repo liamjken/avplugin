3e11c2a063aeec8950b282463c161a64401ebee9
liamjken <liamjken@gmail.com>
2023-01-09 19:47:21 +0000

    moving div to within plugin

--- includes/window-sticker.php
+++ includes/window-sticker.php
@@ -6,7 +6,14 @@ function av_window_sticker_button_function() {
 	?> 
 
 <?php if(!empty($window_sticker)) { ?>	
-             <a style="color: #ffff;font-weight: 600; " href="<?php echo $window_sticker; ?>" target="_blank">Window Sticker</a>
+    <div style="
+    background-color: #005596;
+    text-align: center;
+    padding: 7px 10px;
+    border-radius: 5px;
+    margin-bottom: 5px;
+    margin-top: -2px;
+"><a style="color: #ffff;font-weight: 600; " href="<?php echo $window_sticker; ?>" target="_blank">Window Sticker</a></div>
             <?php } ?>
 
 <?php
